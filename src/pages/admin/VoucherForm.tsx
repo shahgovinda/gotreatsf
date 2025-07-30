d259f0a294e70db7c20b4f607294fa9092a580f6
Govinda J Shah <96227277+shahgovinda@users.noreply.github.com>
2025-07-30 09:57:53 +0530

    Update VoucherForm.tsx

--- src/pages/admin/VoucherForm.tsx
+++ src/pages/admin/VoucherForm.tsx
@@ -227,7 +227,7 @@ const VoucherForm = ({
                                 </div>
                                 <div className='flex gap-4'>
                                     <DateRangePicker
-                                        className=""
+                                        className="bg-white"
                                         label="Start and Expiry Date"
                                         variant='faded'
                                         labelPlacement='outside'
