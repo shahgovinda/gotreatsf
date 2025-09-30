044b66a65b61d3507d7dc8315ee4326660ac1413
Govinda J Shah <96227277+shahgovinda@users.noreply.github.com>
2025-09-30 13:45:32 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,13 @@
 // tailwind.config.js
-const { heroui } = require("@heroui/react");
+const {heroui} = require("@heroui/react");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}", // Added this line to scan your project files
   ],
   theme: {
-    extend: {
-      // --- ADDED ANIMATION CODE START ---
-      animation: {
-        'infinite-scroll': 'infinite-scroll 50s linear infinite',
-      },
-      keyframes: {
-        'infinite-scroll': {
-          from: { transform: 'translateX(0)' },
-          to: { transform: 'translateX(-100%)' },
-        }
-      }
-      // --- ADDED ANIMATION CODE END ---
-    },
+    extend: {},
   },
   darkMode: "class",
   plugins: [heroui()],
