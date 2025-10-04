038b3698e072f7db20ebc29270b27622974982f4
Govinda J Shah <96227277+shahgovinda@users.noreply.github.com>
2025-10-04 11:01:54 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -351,7 +351,7 @@ const Home = () => {
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, ease: "easeOut" }}
-        viewport={{ once: true, amount: 0.2 }}
+        viewport={{ once: true, amount: 0 }} 
         className="py-16 md:py-24 bg-orange-100 mt-10 md:mt-16"
       >
         <div className="container mx-auto px-4 md:px-8 text-center">
